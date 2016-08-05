db7414a1a8074d6f454da1517c1af89aeca87e61
nestorjhernandezm <nestor@steinwurf.com>
2016-08-05 18:39:36 +0200

    Correcting benchmark for Cython optimization

--- benchmark.py
+++ benchmark.py
@@ -19,7 +19,7 @@ import mandelbrot as mnb
 
 methods = ['mnb.mandelbrot_set_naive(Re_c, Im_c)',
            'mnb.mandelbrot_set_vectorized(Real_c + 1j * Imaginary_c)',
-           'mandelbrot_cython(Real_c + 1j * Imaginary_c)']
+           'mandelbrot_set_cython(Real_c + 1j * Imaginary_c)']
 
 points = 500
 
