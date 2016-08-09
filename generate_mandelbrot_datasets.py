6353e12f51a43e738651716da3b9c49d7f727971
nestorjhernandezm <nestor@steinwurf.com>
2016-08-09 18:59:57 +0200

    Number of cores by command line parsing. Log prints to observe script advance. Full datasets

--- generate_mandelbrot_datasets.py
+++ generate_mandelbrot_datasets.py
@@ -1,14 +1,13 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug  7 18:58:40 2016
+Created on Sun Aug  5 18:58:40 2016
 
 @author: nestor
 """
-
+import sys
 from mandelbrot_c import mandelbrot_set_cython
 import numpy as np
 import mandelbrot as mnb
-
 
 # Input parameters
 points = 5000
@@ -20,16 +19,22 @@ Real_c, Imaginary_c = np.meshgrid(Re_c, Im_c)
 
 # Mandelbrot set computations
 M_naive = mnb.mandelbrot_set_naive(Re_c, Im_c)
+print 'Naive implementation computed'
 M_vectorized = mnb.mandelbrot_set_vectorized(Real_c + 1j * Imaginary_c)
+print 'Vectorized implementation computed'
 M_cython = mandelbrot_set_cython(Real_c + 1j * Imaginary_c)
-#M_numba = mnb.mandelbrot_set_numba(Re_c, Im_c)
+print 'Cython implementation computed'
+M_numba = mnb.mandelbrot_set_numba(Re_c, Im_c)
+print 'Numba implementation computed'
 
-cores = 2
+cores = int(sys.argv[1])
 M_multiprocessing = mnb.mandelbrot_set_multiprocessing(Real_c, Imaginary_c,
                                                        cores)
+print 'Multiprocessing implementation computed'
 
+print 'Saving data...'
 np.savez('./mandelbrot_datasets.npz',
          Re_c=Re_c, Im_c=Im_c, Real_c=Real_c, Imaginary_c=Imaginary_c,
          M_naive=M_naive, M_vectorized=M_vectorized, M_cython=M_cython,
-#         M_numba=M_numba, M_multiprocessing=M_multiprocessing)
-         M_multiprocessing=M_multiprocessing)
+         M_numba=M_numba, M_multiprocessing=M_multiprocessing)
+print 'Data saved'
